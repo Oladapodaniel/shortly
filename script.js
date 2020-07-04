7d4725e892fc57f899b55fb18f55256ce1ed9564
Oladapo Daniel <oladapodaniel10@gmail.com>
2020-07-04 23:17:23 +0100

    Project Done. Refurnishing Welcomed

--- script.js
+++ script.js
@@ -41,14 +41,38 @@
 // Posting request in Jquery
 
 $(document).ready(function() {
+
+    var inputco = document.getElementById("input22");
+       
     
+  
    
     //Api call url
 
     let url = 'https://rel.ink/api/links/';
 
-    // Getting user input and 
-// Displaying The UI
+            let table = document.createElement('table');
+            let thead = document.createElement('thead');
+            let thr = document.createElement('tr');
+            let th1 = document.createElement('th');
+            let th2 = document.createElement('th');
+            let th3 = document.createElement('th');
+
+          // Append them into each other
+            thr.appendChild(th3);
+            thr.appendChild(th2);
+            thr.appendChild(th1);
+            thead.appendChild(thr);
+            table.appendChild(thead);
+
+            // The table content
+            th1.textContent = 'Copy';
+            th2.textContent = 'Shortened URL';
+            th3.innerHTML = 'Your Link';
+
+            table.className = 'table table-striped';
+ // Getting user input and 
+ // Displaying The UI
 
     let button = document.querySelector('#button');
     let userInput = document.querySelector('#user-input');
@@ -70,33 +94,60 @@ $(document).ready(function() {
 
             
 
-            let output = document.querySelector('#output');
+            let inputco = document.createElement('input');
 
-            let ul = document.createElement('ul');
-            let li = document.createElement('li');
-            let link = document.createElement('a');
-            ul.className = 'list-group';
-            li.className = 'list-group-item';
-            li.textContent = `https://${userInput.value}`;
-            link.className = 'short-link';
-            link.innerHTML = `https://rel.ink/${data.hashid}  <a href="#" id="copy" data-toggle="tooltip" data-html="true" title="Copy to Clipboard" data-placement="bottom" class="btn cyan text-white">Copy</a>`;
-            li.appendChild(link)
-            ul.appendChild(li);
+
             
-            output.appendChild(ul);
-            document.querySelector('#copy').addEventListener('click', (e) => {
-                userInput.select();
-                userInput.setSelectionRange(0, 99999); // For Mobile Devices
-                document.execCommand('copy');
-                alert('Copied to clipboard')
-                
-            })
+
+            let tbody = document.createElement('tbody');
+            let tr = document.createElement('tr');
+            let td1 = document.createElement('td');
+            let td2 = document.createElement('td');
+            let td3 = document.createElement('td');
+            
+          
+        
            
 
-        })
+            tr.appendChild(td3);
+            tr.appendChild(td2);
+            tr.appendChild(td1);
+            tbody.appendChild(tr);
+            table.appendChild(tbody);
+            output.appendChild(table);
+
+           
+            let ul = document.createElement('ul');
+            let li1 = document.createElement('li');
+            let li2 = document.createElement('li');
+
+            ul.className = 'uk-list uk-list-striped';
+
+            ul.appendChild(li1);
+            ul.appendChild(li2);
+            output1.appendChild(ul)  
+            
+            li1.textContent = `https://${userInput.value}`;
+            li2.innerHTML = `https://rel.ink/${data.hashid}    <a href = "#" id = "copy" data-toggle="tooltip" data-html="true" data-clipboard-text="https://rel.ink/${data.hashid}" title = "Copy to Clipboard" data-placement="bottom" class="btn cyan text-white"> Copy</a>`;
+            
+           
+            td3.textContent = `https://${userInput.value}`;
+            td2.textContent = `https://rel.ink/${data.hashid}`;
+            td2.value = `https://rel.ink/${data.hashid}`;
+            td1.innerHTML = `<a href="#" id="copy" data-toggle="tooltip" data-html="true" data-clipboard-text="https://rel.ink/${data.hashid}" title="Copy to Clipboard" data-placement="bottom" class="btn cyan text-white">Copy</a>`;
+            
+
+
+            e.preventDefault()
+            new ClipboardJS('#copy');
+
+           
+
+
+       
        
     });
- 
+
   
    // let userInput = document.querySelector('#user-input');
     userInput.addEventListener('blur', function () {
@@ -109,7 +160,11 @@ $(document).ready(function() {
 
     })
 
+
+  
+    });
+
+
     $('a[data-toggle="tooltip"]').tooltip() 
 }) 
-
 
